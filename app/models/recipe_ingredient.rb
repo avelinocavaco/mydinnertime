f44a6302d94ca75d2284f59b7619d9726d777cc8
Avelino Cavaco <avelinocavaco@gmail.com>
2026-03-30 22:41:14 +0100

    Added rules to the models to reflect the relationships and validations

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,7 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :preparation, presence: true
+  validates :sequence, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
